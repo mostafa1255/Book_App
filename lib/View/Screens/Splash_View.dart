bfda63c8cb0b00a66752393064587ce82a7b763b
Mostafa <nonatefa11@gmail.com>
2023-04-24 12:59:20 +0200

    first commit

---
+++ lib/View/Screens/Splash_View.dart
@@ -0,0 +1,18 @@
+import 'package:booklyapp/core/Constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+import '../Widgets/Splash_view_Body.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: PrimaryColor,
+      body: SplashViewBody(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:booklyapp/View/Screens/Splash_View.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const BookApp());
+}
+
+class BookApp extends StatelessWidget {
+  const BookApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashView(),
+    );
+  }
+}
