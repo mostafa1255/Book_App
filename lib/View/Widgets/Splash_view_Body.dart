3dadd0eb07411957ec300e320cea6cf917219347
Mostafa <nonatefa11@gmail.com>
2023-04-24 17:28:57 +0200

    Adding Animation to Splash view

--- lib/View/Widgets/Splash_view_Body.dart
+++ lib/View/Widgets/Splash_view_Body.dart
@@ -3,8 +3,29 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> SlidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+
+    SlidingAnimation = Tween<Offset>(begin: Offset(0, 5), end: Offset(0, 0))
+        .animate(animationController);
+
+    animationController.forward();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +40,13 @@ class SplashViewBody extends StatelessWidget {
         SizedBox(
           height: 30,
         ),
-        const Text("Read Free Books")
+        AnimatedBuilder(
+            animation: SlidingAnimation,
+            builder: (context, _) {
+              return SlideTransition(
+                  position: SlidingAnimation,
+                  child: const Text("Read Free Books"));
+            })
       ],
     );
   }
