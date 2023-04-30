bccc9e2710d6e0ffdfb963c4edb59e6713142a89
Mostafa <nonatefa11@gmail.com>
2023-04-30 15:54:42 +0200

    Create Function of Newest Books

--- lib/Features/home/Presentation/Manager/Newest Books/newest_books_cubit.dart
+++ lib/Features/home/Presentation/Manager/Newest Books/newest_books_cubit.dart
@@ -1,10 +1,22 @@
 import 'package:bloc/bloc.dart';
 import 'package:booklyapp/Features/data/models/book_model/book_model.dart';
+import 'package:booklyapp/Features/data/repos/home_repos.dart';
 import 'package:equatable/equatable.dart';
-
 part 'newest_books_state.dart';
 
 class NewestBooksCubit extends Cubit<NewestBooksState> {
-  NewestBooksCubit() : super(NewestBooksInitial());
-  
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> fetchNewestSellerBook() async {
+    emit(NewestBooksLoading());
+
+    var result = await homeRepo.fetchNewestSellerBook();
+
+    result.fold((faliure) {
+      emit(NewestBooksFaliure(faliure.errmessage));
+    }, (books) {
+      emit(NewestBooksSuccsess(books));
+    });
+  }
 }
